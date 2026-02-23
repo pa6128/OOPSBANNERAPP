bb6c0e4da6b26ccab4b11cf1eae4bf76f1c555ce
pa6128 <pa6128@srmist.edu.in>
2026-02-23 15:26:33 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,62 @@
-class OOPSBannerArrayInit {
+import java.util.HashMap;
 
-    public static void main(String[] args) {
+public class OOPSBannerApp {
 
-        String[] banner = {
-			String.join(" ","   ***        ***      ******        ***** "),
-            String.join(" "," **   **    **   **    **    **     **     "),
-            String.join(" ","**     **  **     **   **     **   **     "),
-            String.join(" ","**     **  **     **   **    **     **  "),
-            String.join(" ","**     **  **     **   ******        ***"),
-            String.join(" ","**     **  **     **   **              **  "),
-            String.join(" ","**     **  **     **   **               **     "),
-            String.join(" "," **   **    **    **   **              **       "),
-            String.join(" ","   ***        ***      **         *****   ") 
-        };
+    // Method to create character patterns
+    public static HashMap<Character, String[]> createCharacterMap() {
+        HashMap<Character, String[]> charMap = new HashMap<>();
 
-        
-        for (String line : banner) {
-            System.out.println(line);
+        charMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        charMap.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      "
+        });
+
+        charMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        });
+
+        return charMap;
+    }
+
+    // Method to display banner
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+
+        message = message.toUpperCase();
+        int height = 5;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < message.length(); j++) {
+                char ch = message.charAt(j);
+
+                if (charMap.containsKey(ch)) {
+                    System.out.print(charMap.get(ch)[i] + "  ");
+                } else {
+                    System.out.print("       ");
+                }
+            }
+            System.out.println();
         }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        String message = "OOPS";
+        displayBanner(message, charMap);
     }
 }
